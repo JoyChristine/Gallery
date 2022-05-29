eba8e8a4d6d428ff5f99020fd17b2b946e8f2e8e
Joy Christine <joychristin2@gmail.com>
2022-05-29 14:00:02 +0300

    initial commit

--- art/urls.py
+++ art/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('search/',views.search, name='search'),
     
+    
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
